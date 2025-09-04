3b54797fc5d189e8f87196db6372a9a2dc59baf9
jiminu <jmu417@gmail.com>
2025-09-04 10:15:45 +0000

    add: initializing

--- src/main/java/com/mini/mini_2/Mini2Application.java
+++ src/main/java/com/mini/mini_2/Mini2Application.java
@@ -2,7 +2,9 @@ package com.mini.mini_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+// @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class Mini2Application {
 
