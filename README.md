ba9e46bb388d45098bc71b2a86e4e1b2ee9ab2eb
jiminu <jmu417@gmail.com>
2025-09-04 17:28:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# second mini project
---
+++ src/main/java/com/mini/mini_2/Mini2Application.java
@@ -0,0 +1,13 @@
+package com.mini.mini_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mini2Application.class, args);
+	}
+
+}
