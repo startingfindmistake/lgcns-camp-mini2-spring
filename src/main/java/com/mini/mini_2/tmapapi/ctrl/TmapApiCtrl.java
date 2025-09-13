b96f807b46cc69df3b72d97efb268d793c2d3bab
jiminu <jmu417@gmail.com>
2025-09-13 15:53:48 +0900

    fix: tmap api structure

--- src/main/java/com/mini/mini_2/tmapapi/ctrl/TmapApiCtrl.java
+++ src/main/java/com/mini/mini_2/tmapapi/ctrl/TmapApiCtrl.java
@@ -2,28 +2,31 @@ package com.mini.mini_2.tmapapi.ctrl;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mini.mini_2.tmapapi.domain.dto.RoutePoiRequestDTO;
+import com.mini.mini_2.tmapapi.domain.dto.RoutePoiResponseDTO;
 import com.mini.mini_2.tmapapi.domain.dto.RouteRequestDTO;
 import com.mini.mini_2.tmapapi.domain.dto.RouteResponseDTO;
+import com.mini.mini_2.tmapapi.service.RoutePoiService;
 import com.mini.mini_2.tmapapi.service.RouteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @RestController
-@RequestMapping("api/v2/mini/route")
-public class RouteCtrl {
+@RequestMapping("api/v2/mini/tmap")
+public class TmapApiCtrl {
     
     @Autowired
     private RouteService routeService;
+    
+    @Autowired
+    private RoutePoiService routePoiService;
     
     @PostMapping("route")
     public ResponseEntity<RouteResponseDTO> route(@RequestBody RouteRequestDTO request) {
@@ -38,6 +41,21 @@ public class RouteCtrl {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 
+        }
+    } 
+
+    @PostMapping("poi")
+    public ResponseEntity<RoutePoiResponseDTO> poi(@RequestBody RoutePoiRequestDTO request) {
+        
+        RoutePoiResponseDTO response = routePoiService.poi(request);
+        
+        System.out.println("[TMAP DEBUG] result : " + response);
+        
+        
+        if (response != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     } 
 }
